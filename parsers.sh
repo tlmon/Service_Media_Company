61ca78f43b939688b63b15e0fe8a38266fa9cb46
timlednev <timlednev@mail.ru>
2023-04-20 15:00:05 +0300

    Change bash scripts

--- parsers.sh
+++ parsers.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-pip3 install beautifulsoup4 requests datetime
+w=$(dirname $(readlink -f "$0"))
 
-cd parsers
+mkdir -p $w/data $w/logs
 
-python3 ./rbc_finances_parser.py &
-python3 ./cfo_parser.py &
-python3 ./klerk_parser.py &
-python3 ./consultant_parser.py &&
+cd $w/parsers
+
+time=$(date +'%Y-%m-%d-%H-%M-%S')
+
+python3 ./rbc_finances_parser.py > $w/logs/rbc-finances-parser-${time} 2>&1 &
+python3 ./cfo_parser.py > $w/logs/cfo-parser-${time} 2>&1 &
+# python3 ./klerk_parser.py > $w/logs/klerk-parser-${time} 2>&1 &
+python3 ./consultant_parser.py > $w/logs/consultant-parser-${time} 2>&1 &
 wait
