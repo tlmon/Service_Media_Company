52f46beb8eca18a97977644a7e2d2e5807e62de8
timlednev <timlednev@mail.ru>
2023-04-20 15:10:40 +0300

    Add requrements.sh file

---
+++ requirements.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python3 -m pip install --upgrade pip
+
+#for parsers
+pip3 install beautifulsoup4 requests datetime
+
+#for core
+pip3 installs beautifulsoup4 pandas numpy IPython requests datetime flask nltk pymorphy2 joblib==1.1.0 hdbscan tqdm sklearn
